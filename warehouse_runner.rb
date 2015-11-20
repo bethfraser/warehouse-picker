1a02775ec05b288365b255ee6065428d3035c346
Beth Fraser <bethanyfraser@live.co.uk>
2015-11-20 11:05:32 +0000

    Created first tests in runner and organised data into an array.

---
+++ warehouse_runner.rb
@@ -0,0 +1,18 @@
+require_relative('./warehouse_functions')
+
+####Single bay/item
+
+puts ITEMS.length
+
+# Given a bay, returns the item in that bay
+
+found_item = item_at_bay('b5')
+puts "The item at bay 5 is: #{found_item} (Expect nail filer)"
+puts "\n"
+
+# Given an item return the bay that it is in.
+
+found_bay = bay_for_item('nail filer')
+puts "The bay for item 'nail filer' is: #{found_bay} (Expect b5)"
+puts "\n"
+
